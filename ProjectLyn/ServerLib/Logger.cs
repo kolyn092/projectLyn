afb4e3f1d631128047fee3d16335b8f18c374a54
kosukyung <kosukyung94@gmail.com>
2025-01-07 23:54:50 +0900

    Logger 만드는중

--- ProjectLyn/ServerLib/Logger.cs
+++ ProjectLyn/ServerLib/Logger.cs
@@ -1,7 +1,28 @@
-﻿namespace ServerLib
-{
-    public class Logger
-    {
+﻿using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
+namespace ServerLib
+{
+    public static class Logger
+    {
+        private static ILoggerFactory _loggerFactory;
+        private static LogFactory _nLogFactory;
+        private static ILogger _default;
+
+        public static ILogger Default => _default; // { get; private set; }
+        public static ILoggerFactory LogFactory => _loggerFactory;
+
+        static Logger()
+        {
+
+        }
+
+        public static void Init(string envName)
+        {
+
+        }
     }
 }
