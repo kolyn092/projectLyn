3755c9553b76514a5be11e4ac9f2b643c273ffb2
kosukyung <kosukyung94@gmail.com>
2025-01-10 23:42:37 +0900

    svelte 연결중

--- ProjectLyn/ProjectLyn/Startup.cs
+++ ProjectLyn/ProjectLyn/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
@@ -6,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using ServerLib;
+using System.Text;
 
 namespace ProjectLyn
 {
@@ -22,6 +24,7 @@ namespace ProjectLyn
         {
             Logger.Default.LogDebug("Start ConfigureServices");
             var mvcBuilder = services.AddMvc();
+            mvcBuilder.Services.AddRazorPages();
 
             Logger.Default.LogDebug("Finished ConfigureServices");
         }
@@ -33,14 +36,37 @@ namespace ProjectLyn
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
+
+            //app.UseExceptionHandler(errorApp =>
+            //{
+            //    errorApp.Run(async context =>
+            //    {
+            //        context.Response.StatusCode = 500;
+            //        context.Response.ContentType = "application/json";
+
+
+            //        var exceptionHandlerPathFeature =
+            //            context.Features.Get<IExceptionHandlerPathFeature>();
+
+            //        // Use exceptionHandlerPathFeature to process the exception (for example, 
+            //        // logging), but do NOT expose sensitive error information directly to 
+            //        // the client.
+            //        if (exceptionHandlerPathFeature != null)
+            //        {
+            //            var error = exceptionHandlerPathFeature.Error.ToString();
+            //            Logger.Default.LogError(error);
+            //            var xmlBytes = Encoding.ASCII.GetBytes(error);
+            //            await context.Response.Body.WriteAsync(xmlBytes);
+            //        }
+            //    });
+            //});
 
             // The default HSTS value is 30 days. You may want to change this production scenarios.
             app.UseHsts();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseRouting();
+            app.UseCors(); //web
             app.UseAuthorization();
         }
     }
