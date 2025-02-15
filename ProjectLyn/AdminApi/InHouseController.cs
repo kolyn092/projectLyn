7035027da4a7ec28b84c620760f4470b001da554
kosukyung <kosukyung94@gmail.com>
2025-02-15 23:12:40 +0900

    admin 테스트중

--- ProjectLyn/AdminApi/InHouseController.cs
+++ ProjectLyn/AdminApi/InHouseController.cs
@@ -4,35 +4,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static AdminApi.AdminRequest;
 
-namespace AdminApi
+namespace AdminApi.Controllers
 {
     [ApiController]
     [Route("InHouse/[action]")]
     public class InHouseController : ControllerBase
     {
-        //[HttpPost]
-        //public IActionResult GetData(AdminRequest.SendData data)
-        //{
-        //    var resData = new List<string>();
-        //    if(data.Data == 0)
-        //    {
-        //        resData.Add("test string");
-        //    }
-
-        //    var result = new AdminResponse.GetData()
-        //    {
-        //        Result = new AdminResponse.Result() { Code = 0, Message = string.Empty },
-        //        Data = resData,
-        //    };
-
-        //    return new JsonResult(result);
-        //}
-
-        [HttpPost]
+        [HttpGet]
         public IActionResult GetData()
         {
             return Ok(new { message = "success", data = "test" });
+        }
+
+        [HttpPost]
+        public IActionResult PostData()
+        {
+            return Ok(new { message = "success", data = "test" });
+        }
+
+        [HttpPost]
+        public IActionResult PostData2(SendData data)
+        {
+            var resData = new List<string>();
+            if (data.Data == 0)
+            {
+                resData.Add("PostData2 data is 0");
+            }
+            else
+            {
+                resData.Add("PostData2 data is not 0");
+            }
+
+            var result = new AdminResponse.GetData()
+            {
+                Result = new AdminResponse.Result() { Code = 0, Message = string.Empty },
+                Data = resData,
+            };
+
+            return new JsonResult(result);
+        }
+
+        [HttpPost]
+        public IActionResult PostData3(int data)
+        {
+            var resData = new List<string>();
+            if (data == 0)
+            {
+                resData.Add("PostData3 data is 0");
+            }
+            else
+            {
+                resData.Add("PostData3 data is not 0");
+            }
+
+            var result = new AdminResponse.GetData()
+            {
+                Result = new AdminResponse.Result() { Code = 0, Message = string.Empty },
+                Data = resData,
+            };
+
+            return new JsonResult(result);
         }
     }
 }
--- ProjectLyn/AdminApi/InHouseRequest.cs
+++ ProjectLyn/AdminApi/InHouseRequest.cs
@@ -8,18 +8,7 @@ namespace AdminApi
 {
     public partial class AdminRequest
     {
-        public class Result
-        {
-            public int Code { get; set; }
-            public string Message { get; set; }
-        }
-
-        public class Base
-        {
-            public Result Result { get; set; }
-        }
-
-        public class SendData : Base
+        public class SendData
         {
             public int Data { get; set; }
         }
